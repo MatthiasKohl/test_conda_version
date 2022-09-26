85f6012886041dbf12d5e3ba053911d5473ffec0
Matthias Jouanneaux <matthias.kohl@hotmail.de>
2022-09-26 11:31:10 +0200

    fixed package

---
+++ conda_version_pkg/__init__.py
@@ -0,0 +1,1 @@
+from .conda_version import do
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(name='conda_version_pkg',
-      version='custom',
+      version='0.14a',
       description='Conda version test',
       author='Matthias Jouanneaux',
       author_email='mjoux@nvidia.com',
